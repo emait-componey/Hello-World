c026a8b1b46846711041d3e58ebc00b7dec500d9
emait-componey <144929981+emait-componey@users.noreply.github.com>
2023-09-13 18:50:09 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello World</title>
+  <style>
+    body {
+      display: flex;
+      height: 95vh;
+      justify-content: center;
+      align-items: center;
+      color: #fff;
+      background-color: #0D1117;
+    }
+  </style>
+</head>
+<body>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
