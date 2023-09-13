f039ca480688e5ef0e5d67efe6abafeb5d359a4b
emait-componey <144929981+emait-componey@users.noreply.github.com>
2023-09-13 18:55:42 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Hello World</title>
-  <style>
-    body {
-      display: flex;
-      height: 95vh;
-      justify-content: center;
-      align-items: center;
-      color: #fff;
-      background-color: #0D1117;
-    }
-  </style>
-</head>
 <body>
-<h1>First my php project</h1>
+
+<h1>My first PHP page</h1>
+
 <?php
 echo "Hello World!";
 ?>
